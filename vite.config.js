c9e720320909cca6760847f8e9eb3345b9101d06
Kuldeepkushwah06 <144555844+Kuldeepkushwah06@users.noreply.github.com>
2025-05-14 14:43:07 +0530

    build fix take 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import commonjs from '@vitejs/plugin-commonjs';
+
 
 export default defineConfig({
-	plugins: [react(), tailwindcss(),commonjs()],
+	plugins: [react(), tailwindcss()],
 	optimizeDeps: {
 		include: ["recharts"],
 	},
